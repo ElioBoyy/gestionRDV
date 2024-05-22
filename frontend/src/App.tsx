ee2c9c03ad2f533d3b47881ec2309cb28c929b69
ElioBoyy <mathis.sportiello@gmail.com>
2024-05-23 01:12:52 +0200

    clean up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <>
       <div className='bg-darkGray w-full h-full absolute top-0 left-0 z-0'>
-
+        
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
